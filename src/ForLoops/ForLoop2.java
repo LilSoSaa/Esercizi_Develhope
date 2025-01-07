10703b3b4b5753584e0010110d144a324090c039
Giovanni Topor <122824694+LilSoSaa@users.noreply.github.com>
2025-01-07 14:09:30 +0100

    Update ForLoop2.java

--- src/ForLoops/ForLoop2.java
+++ src/ForLoops/ForLoop2.java
@@ -6,6 +6,26 @@ package ForLoops;
 // uscita [6 5 4] stampati).
 public class ForLoop2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Inserisci un numero che desideri: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Inserisci quanti risultati desideri: ");
+        int numeroRisultati = scanner.nextInt();
+
+        restituisciNumeriN(numero, numeroRisultati);
+
 
     }
+
+    public static void restituisciNumeriN (int numero, int risultatiDesiderati) {
+        System.out.println("I risultati che ci si aspetta sono:  ");
+        for (int i = numero; i >= (numero - risultatiDesiderati + 1) && i >= 0; i--) {
+            System.out.println(i);
+        }
+    }
+
 }
+
+  
